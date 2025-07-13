e946dd5ee8f2a5fb16edacf4e21b3c32413f1173
mituritsyn <mikhail-turicyn@yandex.ru>
2025-07-13 15:29:55 +0200

    add led

--- src/1_lesson/1_lesson.ino
+++ src/1_lesson/1_lesson.ino
@@ -1,6 +1,10 @@
+#define LED_BUILTIN 15
+
 void setup(){
   Serial.begin(115200);
   Serial.println("Hello, World!");
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
   delay(3000);
 }
 
