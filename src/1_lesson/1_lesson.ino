346e9176e487c10eb8fea7cfa92f4a95fd0ac370
mituritsyn <mikhail-turicyn@yandex.ru>
2025-07-14 12:10:08 +0200

    add button

--- src/1_lesson/1_lesson.ino
+++ src/1_lesson/1_lesson.ino
@@ -1,6 +1,8 @@
 #define LED_BUILTIN 15
+#define BOOT_BUTTON 0
 
 void setup(){
+  pinMode(BOOT_BUTTON, INPUT_PULLUP);
   Serial.begin(115200);
   Serial.println("Hello, World!");
   pinMode(LED_BUILTIN, OUTPUT);
@@ -10,5 +12,11 @@ void setup(){
 
 void loop(){
   Serial.println("loop again");
-  delay(1000);
+  if (digitalRead(BOOT_BUTTON) == LOW) {
+    Serial.println("Boot button pressed!");
+  }
+  digitalWrite(LED_BUILTIN, LOW);   // Выключить LED
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);  // Включить LED
+  delay(500);
 }
