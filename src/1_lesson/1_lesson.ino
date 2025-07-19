59a044fc8d30502e858f112599d0810ddde4068b
mituritsyn <mikhail-turicyn@yandex.ru>
2025-07-19 13:12:32 +0200

    finalize ino code

--- src/1_lesson/1_lesson.ino
+++ src/1_lesson/1_lesson.ino
@@ -2,8 +2,13 @@
 
 void setup(){
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.begin(115200);
 }
 
-void loop(){
+void loop(){ 
+  Serial.println("Привет, мир!");
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 }
