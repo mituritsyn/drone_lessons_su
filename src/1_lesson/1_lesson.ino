dbf3c0277439f00a291fea1e31efb190a8951577
mituritsyn <mikhail-turicyn@yandex.ru>
2025-07-22 22:47:51 +0200

    привел код в соответствие с 5 занятием

--- src/1_lesson/1_lesson.ino
+++ src/1_lesson/1_lesson.ino
@@ -1,14 +1,21 @@
-#define LED_BUILTIN 15
+#define LED_PIN 15
+#define BUTTON_PIN 0
 
-void setup(){
-  pinMode(LED_BUILTIN, OUTPUT);
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
   Serial.begin(115200);
 }
 
-void loop(){ 
+void loop() {
   Serial.println("Привет, мир!");
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED_PIN, HIGH);
   delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1500);
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
+  if (digitalRead(BUTTON_PIN)) {
+    Serial.println("Кнопка не нажата!");
+  } else {
+    Serial.println("Кнопка нажата.");
+  }
 }
