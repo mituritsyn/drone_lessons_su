21e80b47a03e04e5f022691f1becb5186f28c11c
mituritsyn <mikhail-turicyn@yandex.ru>
2025-08-24 15:19:02 +0200

    update drone

--- src/drone/drone.ino
+++ src/drone/drone.ino
@@ -1,13 +1,8 @@
-#define pin num
-#define motor1 num1
-#define motor2 num2
-#define motor3 num3
-#define motor4 num4
 
 void setup(){
     Serial.begin(9600);
     setupIMU();
-    initMotors();
+    setupMotors();
     initRadio();
     Serial.println("настройка завершена");
 }
--- src/drone/motors.ino
+++ src/drone/motors.ino
@@ -1,7 +1,20 @@
-void initMotors(){
-    // тут мы будем инициализировать моторы
+#define MOTORS_COUNT 4
+#define PWM_RESOLUTION 12
+#define PWM_FREQUENCY 80000000/2^PWM_RESOLUTION
+// 1<<PWM_RESOLUTION
+
+int motorsPower[MOTORS_COUNT];
+int motorPins[] = {1,2,3,4};
+
+void setupMotors(){
+    for(int i=0; i < MOTORS_COUNT; i++){
+        // pinMode
+        ledcAttach(motorPins[i], PWM_FREQUENCY, PWM_RESOLUTION);
+    }
 }
  void spinMotor(){
-    // кручение моторов
-    analogWrite(MOTOR1,power1);
+    for(int i=0; i < MOTORS_COUNT; i++){
+        // analogWrite
+        ledcWrite(motorPins[i], motorsPower[i]);
+    }
  }
